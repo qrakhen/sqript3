bf9e6a07faa3ab7f48876c4f6486f9a5ecb21319
Qrakhen <filosofem.dave@gmail.com>
2022-12-17 23:36:30 +0100

    kasdkads

--- compiler.c
+++ compiler.c
@@ -6,4 +6,16 @@
 
 void compile(const char* source) {
     initReader(source);
+    int line = -1;
+    do {
+        Token token = scanToken();
+        if (token.line != line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        }
+        else {
+            printf(" | ");
+        }
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+    } while (token.type != TOKEN_EOF);
 }
--- reader.c
+++ reader.c
@@ -16,3 +16,54 @@ void initReader(const char* source) {
     reader.cursor = source;
     reader.line = 0;
 }
+
+static bool readerDone() {
+    return *reader.cursor == '\0';
+}
+
+static Token makeToken(TokenType type) {
+    Token t;
+    t.type = type;
+    t.start = reader.start;
+    t.length = (int)(reader.cursor - reader.start);
+    t.line = reader.line;
+    return t;
+}
+
+static Token makeError(const char* message) {
+    Token t;
+    t.type = TOKEN_ERROR;
+    t.start = message;
+    t.length = (int)(strlen(message));
+    t.line = reader.line;
+    return t;
+}
+
+static char __next() {
+    return *(reader.cursor++);
+}
+
+Token readToken() {
+    reader.start = reader.cursor;
+    if (readerDone()) return makeToken(TOKEN_EOF);
+
+    char c = __next();
+
+    switch (c) {
+        case '(': return makeToken(TOKEN_GROUP_OPEN);
+        case ')': return makeToken(TOKEN_GROUP_CLOSE);
+        case '{': return makeToken(TOKEN_CONTEXT_OPEN);
+        case '}': return makeToken(TOKEN_CONTEXT_CLOSE);
+        case '[': return makeToken(TOKEN_COLLECTION_OPEN);
+        case ']': return makeToken(TOKEN_COLLECTION_CLOSE);
+        case ';': return makeToken(TOKEN_SEMICOLON);
+        case ',': return makeToken(TOKEN_COMMA);
+        case '.': return makeToken(TOKEN_DOT);
+        case '+': return makeToken(TOKEN_PLUS);
+        case '-': return makeToken(TOKEN_MINUS);
+        case '/': return makeToken(TOKEN_SLASH);
+        case '*': return makeToken(TOKEN_STAR);
+    }
+
+    return makeError("unexpected character: %s", reader.start[0]);
+}
