b9bd111233acd30035d5169604adb8e5432ca77f
Qrakhen <filosofem.dave@gmail.com>
2022-12-17 16:20:15 +0100

    things

--- common.h
+++ common.h
@@ -12,6 +12,11 @@
 
 #define DEBUG_MODE
 
+#define E_ERR_SQR_COMPILE 65
+#define E_ERR_SQR_RUNTIME 70
+#define E_ERR_IO_NOFILE 74
+#define E_ERR_IO_BADFILE 75
+
 static char* F(char* str, char* value) {
     char _[64];
     sprintf(_, str, value);
--- main.c
+++ main.c
@@ -7,24 +7,55 @@
 #include "log.h"
 #include "runtime.h"
 
-#define write writeSegment
-#define init initSegment
-#define clear clearSegment
-#define digest digestSegment
+static void runConsole() {
+    char line[1024];
+    for (;;) {
+        printf("> ");
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+        interpret(line);
+    }
+}
+
+static void runFile(const char* path) {
+    char* source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
+    if (result == INTERPRET_COMPILE_ERROR) exit(E_ERR_SQR_COMPILE);
+    if (result == INTERPRET_RUNTIME_ERROR) exit(E_ERR_SQR_RUNTIME);
+}
+
+static char* readFile(const char* path) {
+    FILE* file = fopen(path, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "could not find file: \"%s\".\n", path);
+        exit(E_ERR_IO_NOFILE);
+    }
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);
+    char* buffer = (char*)malloc(fileSize + 1);
+    if (bytesRead < fileSize) {
+        fprintf(stderr, "file seems to be corrupt: \"%s\".\n", path);
+        exit(E_ERR_IO_BADFILE);
+    }
+    buffer[bytesRead] = '\0';
+    fclose(file);
+    return buffer;
+}
 
 int main(int argc, const char* argv[])
 {
     initRuntime();
-    Segment seg;
-    init(&seg);
-    write(&seg, OP_CONSTANT, 0);
-    write(&seg, registerConstant(&seg, 111), 0);
-    write(&seg, OP_CONSTANT, 0);
-    write(&seg, registerConstant(&seg, 222), 0);
-    write(&seg, OP_ADD, 0);
-    write(&seg, OP_RETURN, 0);
-    interpret(&seg);
+
+    if (argc == 1) {
+        runConsole();
+    } else if (argc > 1) {
+        runFile(argvs[1]);
+    }
+
     clearRuntime();
-    clear(&seg);
     return 0;
 }
