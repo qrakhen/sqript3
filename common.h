187c06ab962292d87893b484e718275922e4763b
Qrakhen <filosofem.dave@gmail.com>
2023-01-10 03:16:11 +0100

    some swag

--- common.h
+++ common.h
@@ -23,9 +23,9 @@
 #define __DBG_PRINT_STATEMENTS false
 #define __DBG_PRINT_EXEC_TIME true
 #define __DBG_SHOW_FULL_INFO false
-#define __DBG_STACK true
+#define __DBG_STACK false
 #define __DBG_TRACE false
-#define __DBG_GCLOG false
+#define __DBG_GCLOG true
 
 #define __CLI_SHOW_TIME false
 
@@ -50,8 +50,22 @@
 #define SQR_OPTION_FLAG_FORCE_TYPES 32
 #define SQR_OPTION_FLAG_NO_ALIASES 64
 #define SQR_OPTION_FLAG_COMPILE_ONLY 128
-#define SQR_OPTION_FLAG_CONTINUE_ON_PANIC 256
-#define SQR_OPTION_FLAG_COLORED_CLI 512
+#define SQR_OPTION_FLAG_READ_ONLY 256
+#define SQR_OPTION_FLAG_CONTINUE_ON_PANIC 512
+#define SQR_OPTION_FLAG_COLORED_CLI 1024
+#define SQR_OPTION_FLAG_ENABLED(flag) (SQR_OPTION_FLAGS & flag > 0)
+
+int SQR_OPTION_FLAGS;
+
+#define SQR_DBG_FLAG_PRINT_STATEMENTS 1
+#define SQR_DBG_FLAG_PRINT_EXEC_TIME 2
+#define SQR_DBG_FLAG_SHOW_FULL_INFO 4
+#define SQR_DBG_FLAG_STACK 8
+#define SQR_DBG_FLAG_TRACE 16
+#define SQR_DBG_FLAG_GCLOG 32
+#define SQR_DBG_FLAG_ENABLED(flag) (SQR_DEBUG_FLAGS & flag > 0)
+
+int SQR_DEBUG_FLAGS;
 
 #define TIME_UNIT_NS 0
 #define TIME_UNIT_MS 1
@@ -66,6 +80,9 @@
 #define TIME ((unsigned long)time());
 
 #define F formatToString
+
+
+bool optionEnabled(int optionFlag);
 
 char* formatToString(char* format, ...);
 char* formatTime(double ms, int unit);
